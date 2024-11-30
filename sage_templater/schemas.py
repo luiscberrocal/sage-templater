f040f51d7083e1de3ff2fb87764a737bf1e22b57
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-11-30 14:57:04 -0500

    Parse 'None' from tax.

--- sage_templater/schemas.py
+++ sage_templater/schemas.py
@@ -1,17 +1,19 @@
 from decimal import Decimal
 from typing import Annotated, AnyStr, Optional
 
-from pydantic import AfterValidator, BaseModel
+from pydantic import AfterValidator, BaseModel, BeforeValidator
 
 
 def validate_tax(value: AnyStr):
     """Validate the tax value."""
-    if value is None or value == "" or value == "None":
+    if isinstance(value, tuple):
+        value = value[0]
+    if value is None or len(value) == 0 or value == "None":
         return Decimal("0.0")
     return Decimal(value)
 
 
-Tax = Annotated[Decimal, AfterValidator(validate_tax)]
+Tax = Annotated[Decimal, BeforeValidator(validate_tax)]
 
 
 class SmallBoxRecordSchema(BaseModel):
@@ -24,7 +26,7 @@ class SmallBoxRecordSchema(BaseModel):
     invoice: Optional[str] = None
     date: str
     amount: Decimal
-    tax: Optional[Tax] = Decimal("0.0")
+    tax: Tax
     total: Decimal
     description: str
     source_file: Optional[str] = None
