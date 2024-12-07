1bbd28d5ef78eb43fbf899426629cfbed75d75ec
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-12-07 08:13:52 -0500

    Finding templates

--- sage_templater/plugins/file_classifiers/excel_classifier.py
+++ sage_templater/plugins/file_classifiers/excel_classifier.py
@@ -2,7 +2,10 @@ from pathlib import Path
 from time import time
 from typing import List
 
+import click
+
 from sage_templater.plugins.parsers.excel_parser import is_small_box_template
+
 
 def get_excel_files(folder: Path, pattern: str = "**/*.xlsx") -> List[Path]:
     """Get excel files from a folder."""
@@ -17,13 +20,12 @@ def main():
             start = time()
             is_small_box = is_small_box_template(excel_file)
             elapsed = time() - start
-            if is_small_box or "caja" in excel_file.name.lower():
-                print(f"{i + 1} {excel_file.relative_to(folder)} {is_small_box} time: {elapsed:.2f}")
+            # if is_small_box or "caja" in excel_file.name.lower():
+            click.secho(f"{i + 1} {excel_file.relative_to(folder)} {is_small_box} time: {elapsed:.2f}", fg="green")
 
         except Exception as e:
-            print(f"{i + 1} {excel_file.relative_to(folder)} {e}")
+            click.secho(f"{i + 1} {excel_file.relative_to(folder)} {e}", fg="red")
+
 
 if __name__ == '__main__':
     main()
-
-
