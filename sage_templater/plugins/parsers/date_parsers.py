fb0c2137fff8501605f74b8730e222851bc1031a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-12-08 07:54:25 -0500

    Improved date parsing

--- sage_templater/plugins/parsers/date_parsers.py
+++ sage_templater/plugins/parsers/date_parsers.py
@@ -1,20 +1,39 @@
+import re
 from datetime import datetime
 
 from dateutil.parser import parse
 
 
-def parse_date(date_str: str) -> datetime | None:
+def parse_date_old(date_str: str) -> datetime | None:
     try:
-        dt = parse_spanish_date(date_str)
-        if dt:
-            return dt
-        return parse(date_str)
+        regexp = re.compile(r"\d{4}-\d{2}-\d{4}")
+        if regexp.match(date_str):
+            return datetime.strptime(date_str, "%Y-%m-%d")
+
+        return parse(date_str, dayfirst=True)
     except ValueError:
         # Handle errors, perhaps log them or return a default value
         return None
+
 
 def parse_spanish_date(date_str: str) -> datetime | None:
     try:
         return datetime.strptime(date_str, "%d/%m/%Y")
     except ValueError:
         return None
+
+
+def parse_date(str_date: str) -> datetime | None:
+    datetime_regexp_strs = [
+        (r"(?P<year>[0-9]{4})-(?P<month>[\d]{1,2})-(?P<day>[\d]{1,2})", "%Y-%m-%d"),
+        (r"(?P<day>[\d]{1,2})/(?P<month>[\d]{1,2})/(?P<year>[0-9]{4})", "%d/%m/%Y"),
+        (r"(?P<day>[\d]{1,2})/(?P<month>[\d]{1,2}))/(?P<year>[0-9]{2})", "%d/%m/%y"),
+        (r"(?P<year>[0-9]{4})-(?P<month>1[0-2]|0[1-9])-(?P<day>3[01]|[12][0-9]|0[1-9]) "
+         r"(?P<hour>2[0-3]|[01][0-9]):(?P<min>[0-5][0-9]):(?P<sec>[0-5][0-9])",
+         "%Y-%m-%d %H:%M:%S"),
+    ]
+    for datetime_regexp_str, format_str in datetime_regexp_strs:
+        regexp = re.compile(datetime_regexp_str)
+        match = regexp.match(str_date)
+        if match is not None:
+            return datetime.strptime(str_date, format_str)
--- tests/unit/plugins/parsers/test_date_parsers.py
+++ tests/unit/plugins/parsers/test_date_parsers.py
@@ -9,10 +9,11 @@ class TestDateParsers:
 
 
     @pytest.mark.parametrize("str_date, expected_date", [
-        #("2022-01-02", datetime(2022, 1, 2)),
-        # ("2022-01-02 12:00:00", datetime(2022, 1, 2, 12, 0, 0)),
-        ("2/1/2022", datetime(2022, 1, 2, 12, 0, 0)),
+        ("2022-01-02", datetime(2022, 1, 2)),
+        ("2022-1-2", datetime(2022, 1, 2)),
+        ("2/1/2022", datetime(2022, 1, 2)),
         ("18/05/2021", datetime(2021, 5, 18)),
+        ("18/05/21", datetime(2021, 5, 18)),
     ])
     def test_parse_date(self, str_date, expected_date):
         date = parse_date(str_date)
