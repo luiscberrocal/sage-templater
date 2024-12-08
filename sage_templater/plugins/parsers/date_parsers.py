2c5e3c17fa056fcf2bf1436efd01093a904a216b
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-12-08 08:20:33 -0500

    Date parser working for spanish dates

--- sage_templater/plugins/parsers/date_parsers.py
+++ sage_templater/plugins/parsers/date_parsers.py
@@ -23,11 +23,14 @@ def parse_spanish_date(date_str: str) -> datetime | None:
         return None
 
 
-def parse_date(str_date: str) -> datetime | None:
+def parse_date(str_date: str, raise_error:bool=False) -> datetime | None:
+   # "(?P<day>[\d]{1,2})/(?P<month>[\d]{1,2})/(?P<year>[\d]{2})"
     datetime_regexp_strs = [
         (r"(?P<year>[0-9]{4})-(?P<month>[\d]{1,2})-(?P<day>[\d]{1,2})", "%Y-%m-%d"),
         (r"(?P<day>[\d]{1,2})/(?P<month>[\d]{1,2})/(?P<year>[0-9]{4})", "%d/%m/%Y"),
-        (r"(?P<day>[\d]{1,2})/(?P<month>[\d]{1,2}))/(?P<year>[0-9]{2})", "%d/%m/%y"),
+        (r"(?P<day>[\d]{1,2})-(?P<month>[\d]{1,2})-(?P<year>[0-9]{4})", "%d-%m-%Y"),
+        (r"(?P<day>[\d]{1,2})/(?P<month>[\d]{1,2})/(?P<year>[\d]{2})", "%d/%m/%y"),
+        (r"(?P<day>[\d]{1,2})-(?P<month>[\d]{1,2})-(?P<year>[\d]{2})", "%d-%m-%y"),
         (r"(?P<year>[0-9]{4})-(?P<month>1[0-2]|0[1-9])-(?P<day>3[01]|[12][0-9]|0[1-9]) "
          r"(?P<hour>2[0-3]|[01][0-9]):(?P<min>[0-5][0-9]):(?P<sec>[0-5][0-9])",
          "%Y-%m-%d %H:%M:%S"),
@@ -36,4 +39,11 @@ def parse_date(str_date: str) -> datetime | None:
         regexp = re.compile(datetime_regexp_str)
         match = regexp.match(str_date)
         if match is not None:
-            return datetime.strptime(str_date, format_str)
+            try:
+                return datetime.strptime(str_date, format_str)
+            except ValueError:
+                if raise_error:
+                    raise
+                return None
+
+    return None
